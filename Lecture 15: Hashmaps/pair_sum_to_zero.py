af7eafab4769e8f904910d929976d02edcce2d90
Diptanu Saha <diptanusaha2001@gmail.com>
2023-04-30 11:12:52 +0530

    Create pair_sum_to_zero.py

---
+++ Lecture 15: Hashmaps/pair_sum_to_zero.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+def pairSum0(l,n):
+    # Write your code here
+    pass
+                
+    
+
+
+
+
+
+
+
+
+
+
+    
+def takeInput():
+    #To take fast I/O
+    n=int(stdin.readline().strip())
+    if n==0:
+        return list(),0
+    arr=list(map(int,stdin.readline().strip().split( )))
+    return arr,n
+
+arr,n=takeInput()
+print(pairSum0(arr,n))
