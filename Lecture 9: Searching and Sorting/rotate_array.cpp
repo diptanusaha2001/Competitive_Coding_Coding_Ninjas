c998a93751aa30673c848cb16ee3d3c339f5449a
Diptanu Saha <diptanusaha2001@gmail.com>
2023-05-01 21:28:13 +0530

    Create rotate_array.cpp

---
+++ Lecture 9: Searching and Sorting/rotate_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void rotate(int arr[], int n, int d) {
+  int temp[d];
+  for (int i = 0; i < d; i++) {
+    temp[i] = arr[i];
+  }
+  for (int i = d; i < n; i++) {
+    arr[i - d] = arr[i];
+  }
+  for (int i = 0; i < d; i++) {
+    arr[n - d + i] = temp[i];
+  }
+}
