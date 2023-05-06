041085372af5fd9c2c3bdf8cbda23da11f3c7d3b
Diptanu Saha <diptanusaha2001@gmail.com>
2023-05-06 09:56:46 +0530

    Create reverse_each_word.py

---
+++ Lecture 10: Character Arrays and 2D Arrays/reverse_each_word.py
@@ -0,0 +1,14 @@
+def reverseEachWord(str):
+    li = str.split(' ')
+    size = len(li)
+
+    for i in range(size):
+        s = li[i]
+        oSize = len(s)
+        for j in range(oSize-1, -1, -1):
+            print(s[j], end='')
+        print('', end=' ')
+
+
+str = input()
+reverseEachWord(str)
